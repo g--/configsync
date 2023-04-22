7880377c860b00f75b787af54e04cd54f045e7b3
Geoff Oakham <geoff.oakham@swingtix.ca>
2023-04-22 15:09:33 -0400

    snap and stuff

--- bin.Linux/install_configsync_noedit.sh
+++ bin.Linux/install_configsync_noedit.sh
@@ -2,3 +2,6 @@
 
 
 mkdir -p ~/.configsync && wget -O - https://github.com/g--/configsync/archive/refs/heads/master.tar.gz | tar -xzf - --strip-components 1 -C ~/.configsync
+
+echo "echo '. ~/.configsync/bashrc' >> ~/.bashrc"
+echo ". ~/.configsync/bashrc"
