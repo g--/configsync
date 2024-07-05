ef434e8e50be1d311e84db6f65340fe04d63ae5b
Geoff Oakham <geoff.o@ecobee.com>
2024-07-05 15:52:20 -0400

    forgotten lib filE

---
+++ lib/pandoc_output_backticks_always.lua
@@ -0,0 +1,10 @@
+-- borrowed from https://stackoverflow.com/questions/66945893/use-fenced-code-blocks-in-pandoc-markdown-output
+local fenced = '```\n%s\n```\n'
+function CodeBlock (cb)
+  -- use pandoc's default behavior if the block has classes or attribs
+  if cb.classes[1] or cb.attributes[1] then
+    return nil
+  end
+  return pandoc.RawBlock('markdown', fenced:format(cb.text))
+end
+
