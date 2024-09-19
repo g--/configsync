8d89751dadd9037225424403dcf4fa77a8c7c648
Geoff <geoff.o@ecobee.com>
2024-09-19 10:26:51 -0400

    feat: ✨ `s` has url links

---
+++ fish/functions/_url.fish
@@ -0,0 +1,7 @@
+
+function _url
+	# TODO adjust origin/main
+	printf '\e]8;;%s\e\\\\%s\e]8;;\e' $argv[1] $argv[2]
+end
+
+
--- fish/functions/s.fish
+++ fish/functions/s.fish
@@ -1,37 +1,61 @@
 
 function s 
 	# TODO adjust origin/main
+	# TODO parallel execution for snappier response?
+
+	set ticket (_ticket)
+	if [ "$ticket" != "" ]; and [ "$JIRA_BASE" != "" ]
+		set_color --bold black
+		echo "Ticket"
+		set_color normal
+
+		echo -n "   "
+		jira view --template=title $ticket
+		echo ""
+	end
 
 	set BRANCH (git branch --show-current)
 	if [ "$BRANCH" != "" ]
-		echo $BRANCH
+		set_color --bold black
+		echo "Branch"
+		set_color normal
+		echo "   " $BRANCH
+		echo ""
 	else
-		echo "Not on a branch"
+		echo "(no branch)"
 	end
 
 	# commit stack here
-	set COMMITS (git log --format=reference origin/main..HEAD)
+	set COMMITS (git log --format=reference --color=always origin/main..HEAD)
 
 	if [ "$COMMITS" != "" ]
-		echo $COMMITS
+		set_color --bold black;
+		echo "Commits"
+		set_color normal
+		printf %s\n $COMMITS
 
 		# changed files in commits
 		git diff --stat origin/main HEAD
 		echo ""
 	end
 
-	set UNCOMMITED ( git status -s )
+	set UNCOMMITTED ( git status -s )
 	if [ "$UNCOMMITTED" != "" ]
-		echo $UNCOMMITTED
+		set_color --bold black;
+		echo "Uncommitted"
+		set_color normal
+		printf %s\n $UNCOMMITTED
 		echo ""
 	end
 
 	# is there an open PR?, 
 	gh pr view --json state,reviewDecision,url,number 2>/dev/null | jq -r '.state, .reviewDecision, .url, .number' | read -L -l STATE DECISION URL NUMBER
 	if [ "$NUMBER" != "" ]
-	    echo "pr $NUMBER is in $STATE / $DECISION -- see $URL"
-		# STATE is either OPEN or MERGED
-		# DECISION is either "", "APPROVED", "REVIEW_REQUIRED", or "??"
+		set_color --bold black;
+		echo "Pull Request"
+		set_color normal
+		echo '    ' (_url "$URL" "pr $NUMBER is in $STATE / $DECISION")
+		#printf '    %s\e]8;;%s\e\\\\%s\e]8;;\e\\\\\n' "$URL" "pr $NUMBER is in $STATE / $DECISION"
 	end
 end
 
