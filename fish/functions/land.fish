0972f7ac4ffc6b3dbf5a2c43f6da608a63c86bfc
Geoff <geoff.o@ecobee.com>
2025-02-05 11:47:55 -0500

    feat: ✨ land

---
+++ fish/functions/land.fish
@@ -0,0 +1,21 @@
+function land
+    rebase
+    set BASE (git merge-base (_main) HEAD)
+	git log $BASE..HEAD
+
+	while true
+		read -l -P 'Ready to land? [y/r/N] ' confirm
+
+		switch $confirm
+		  case Y y
+			gh pr merge --rebase --auto
+			return 0
+		  case R r
+		    rebase_interactive
+			continue
+		  case '' N n
+			return 1
+		end
+	end
+end
+
